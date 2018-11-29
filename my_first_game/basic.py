8fb05b758425374e378df9472ad226ca9f902490
shilinlee <836160610@qq.com>
2018-11-29 16:55:16 +0800

    the definition of RGB

--- my_first_game/basic.py
+++ my_first_game/basic.py
@@ -2,19 +2,26 @@ import pygame
 
 pygame.init()
 
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+
+
 gameDisplay = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Slither")
 
-pygame.display.update()
 
 gameExit = False
 
 while not gameExit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            print(event)
             gameExit = True
-        # print(event)
+
+    gameDisplay.fill(white)
+    pygame.display.update()
+
+
 
 pygame.quit()
 
