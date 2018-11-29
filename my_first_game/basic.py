55b029964d812fdf081717620120e59f6fac8f44
shilinlee <836160610@qq.com>
2018-11-29 16:31:56 +0800

    learn something about pygame event

--- my_first_game/basic.py
+++ my_first_game/basic.py
@@ -3,8 +3,15 @@ import pygame
 pygame.init()
 
 gameDisplay = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Slither")
 
 pygame.display.update()
+
+gameExit = False
+
+while not gameExit:
+    for even in pygame.event.get():
+        print(even)
 
 pygame.quit()
 
