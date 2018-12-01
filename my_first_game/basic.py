85f0e03082e04bad190728d9144243c86f6d2570
shilinlee <836160610@qq.com>
2018-12-01 17:21:24 +0800

    More apples and snake function

--- my_first_game/basic.py
+++ my_first_game/basic.py
@@ -6,8 +6,8 @@ pygame.init()
 
 white = (255, 255, 255)
 black = (0, 0, 0)
-
 red = (255, 0, 0)
+green = (0, 155, 0)
 
 display_width = 800
 display_height = 600
@@ -23,6 +23,12 @@ block_size = 10
 FPS = 30
 
 font = pygame.font.SysFont(None, 25)
+
+
+def snake(lead_x, lead_y, block_size):
+    
+    pygame.draw.rect(gameDisplay, green, [lead_x, lead_y, block_size, block_size])
+    
 
 
 def message_to_screen(msg, color):
@@ -75,18 +81,20 @@ def gameLoop():
                     lead_x_change = 0
                     lead_y_change = block_size
 
-        if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0:
+        if lead_x >= display_width or lead_x < 0 or lead_y >= display_height or lead_y < 0:           
             gameOver = True
 
         lead_x += lead_x_change
         lead_y += lead_y_change
         gameDisplay.fill(white)
         pygame.draw.rect(gameDisplay, red, [randAppleX, randAppleY, block_size, block_size])
-        pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, block_size, block_size])
+        snake(lead_x, lead_y, block_size)
         pygame.display.update()
 
         if lead_x == randAppleX and lead_y == randAppleY:
-            print("om nom nom")
+             randAppleX = round(random.randrange(0, display_width-block_size)/10.0) * 10
+             randAppleY = round(random.randrange(0, display_height-block_size)/10.0) * 10
+
 
         clock.tick(FPS)
 
