2982ee1a1ae1929fb013ade7e08d88b245912420
shilinlee <836160610@qq.com>
2018-12-01 11:17:48 +0800

    let the dot-snake up and down

--- my_first_game/basic.py
+++ my_first_game/basic.py
@@ -14,7 +14,9 @@ gameExit = False
 
 lead_x = 300
 lead_y = 300
+
 lead_x_change = 0
+lead_y_change = 0
 
 clock = pygame.time.Clock()
 
@@ -26,15 +28,24 @@ while not gameExit:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 lead_x_change = -10
-            if event.key == pygame.K_RIGHT:
+                lead_y_change = 0
+            elif event.key == pygame.K_RIGHT:
                 lead_x_change = 10
+                lead_y_change = 0
+            elif event.key == pygame.K_UP:
+                lead_x_change = 0
+                lead_y_change = -10
+            elif event.key == pygame.K_DOWN:
+                lead_x_change = 0
+                lead_y_change = 10
 
     lead_x += lead_x_change
+    lead_y += lead_y_change
     gameDisplay.fill(white)
     pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, 10, 10])
     pygame.display.update()
 
-    clock.tick(15)
+    clock.tick(30)
 
 
 pygame.quit()
